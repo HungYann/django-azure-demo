41b0b77ecf0b98e6a5ddf160d6d15ba200ff059a
LIU-HONGYANG <1664698982@qq.com>
2019-11-14 12:58:19 +0800

    last change

--- django_azure_demo/settings.py
+++ django_azure_demo/settings.py
@@ -121,11 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = ''
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
-STATICFILES_DIRS = (
 
-    ('assets', os.path.join(PROJECT_DIR, '../static')),
-)
+
